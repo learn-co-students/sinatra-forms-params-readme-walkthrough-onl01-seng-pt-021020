b06799e265eec11fe6fe0b55914b2225a3f9a0c0
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-05-05 22:09:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ class App < Sinatra::Base
   get '/food_form' do
     erb :food_form
   end
+  
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+  end 
+  
+  #Let's use the key-value pairs in params to return the following phrase, using good-old string interpolation^^
 
-  # Add your post route and action below
+  
 
 end
+
+#We are going to connect HTML forms to a Sinatra application by building a form that takes a user's name and favorite food and returns an interpolated string. 
